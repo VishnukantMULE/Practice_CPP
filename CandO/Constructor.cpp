e074a52eaafaa5d3d6fc873d4375d0a356de7d90
Vishnukant MUle <92650489+VishnukantMULE@users.noreply.github.com>
2023-12-06 03:42:15 +0530

    Create Constructor.cpp

---
+++ CandO/Constructor.cpp
@@ -0,0 +1,24 @@
+//Constructor
+
+
+//it same name as class name
+//it run when object is created
+//it does not have return type
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class person
+{
+public:
+	person()
+	{
+		cout<<"Hello Person"<<endl;
+	}
+	
+};
+int main()
+{
+	person pn;
+	return 0;
+}
