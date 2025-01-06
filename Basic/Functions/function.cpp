766f4ecdcaa842798ef371458ebe247fb494ee1b
vishnukant.mule <vishnukant.mule@finoux.com>
2025-01-06 16:14:59 +0530

    Function MD

---
+++ Basic/Functions/function.cpp
@@ -0,0 +1,34 @@
+/*
+:: Functions 
+
+1. A function is a block of code designed to perform a specific task.
+2. It takes input, processes it, and produces output.
+3. Functions help avoid repetitive code by allowing reuse for different inputs.
+4. Functions run only when they are called.
+
+>> Functions may have different return types:
+
+   - `bool <function_name>`
+   - `int <function_name>`
+   - `void <function_name>`
+   - `string <function_name>`
+
+>> Types of Functions:
+
+1. **Parameterized Functions**: These functions take parameters (e.g., `function_name(z, y)`).
+2. **Unparameterized Functions**: These functions do not take any parameters (e.g., `function_name()`).
+
+> **Actual Parameters**  
+The parameters passed to the function are called **Actual Parameters**.  
+Example:  
+`isGreat(5, 10);`
+
+> **Formal Parameters**  
+The parameters received by the function are called **Formal Parameters**.  
+Example:  
+```c++
+bool isGreat(int a, int b) {
+    return true;
+}
+
+*/
